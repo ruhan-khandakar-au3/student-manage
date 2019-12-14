6ca4c46a24bd8abdf43d134df7108e5c401a3c3d
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-12-14 20:24:06 +0530

    feat: add animation for slider hide and show

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import { useSelector } from "react-redux";
+import { CSSTransition } from "react-transition-group";
 
 import Home from "./pages/Home";
 import Header from "./components/header/Header";
@@ -24,15 +25,16 @@ const App = () => {
             <div className="col s12 m8 offset-m2 l3 d-block">
               <SideBar />
             </div>
-            {!hidden && (
-              <div
-                className={`col s12 m8 offset-m2 l3 db-none ${
-                  hidden ? "slide-out" : "slide-in"
-                }`}
+            <div className={`col s12 m8 offset-m2 l3 db-none `}>
+              <CSSTransition
+                timeout={400}
+                classNames="slide"
+                in={!hidden}
+                appear={!hidden}
               >
                 <SideBar />
-              </div>
-            )}
+              </CSSTransition>
+            </div>
             <div className="col s12 m8 offset-m2 l9">
               <div className="right-content ">
                 <Home />
